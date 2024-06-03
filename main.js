31b9145d88bac858b9fabc9463ba2856b2e370c5
yantowsky <it.yantowsky@gmail.com>
2024-06-03 22:37:37 +0300

    Created HomeWork 9.1

---
+++ main.js
@@ -0,0 +1,22 @@
+let company = {
+    sales: [{ name: 'John', salary: 1000 }, { name: 'Alice', salary: 600 }],
+    development: {
+        web: [{ name: 'Peter', salary: 2000 }, { name: 'Alex', salary: 1800 }],
+        internals: [{ name: 'Jack', salary: 1300 }]
+    }
+};
+
+let totalSalary = 0;
+
+function calcTotalSalary (company) {
+    for (let key in company) {
+        if (typeof (company[key]) === 'object') {
+            calcTotalSalary(company[key]);
+        } else if (key === 'salary') {
+            totalSalary += company[key];
+        }
+    }
+    return totalSalary;
+}
+
+console.log(calcTotalSalary(company));
